ff79afdfd5ad59080f74aa6d41ad7355e35bbef6
SerogaVoin <sergvoin0904@gmail.com>
2023-08-28 23:22:36 +0300

    Selection Obj
    Create selection script
    Create selection obj

---
+++ Assets/Scripts/PickUpObj.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpObj : MonoBehaviour
+{
+    public GameObject camera;
+    public float distance = 15f;
+    GameObject currentWeapon;
+    bool canPickUp = false;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) PickUp();
+        if (Input.GetKeyDown(KeyCode.Q)) Drop();
+    }
+
+    void PickUp()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, distance))
+        {
+            if (hit.transform.tag == "Obj")
+            {
+                if (canPickUp) Drop();
+
+                currentWeapon = hit.transform.gameObject;
+                currentWeapon.GetComponent<Rigidbody>().isKinematic = true;
+                currentWeapon.GetComponent<Collider>().isTrigger = true;
+                currentWeapon.transform.parent = transform;
+                currentWeapon.transform.localPosition = Vector3.zero;
+                currentWeapon.transform.localEulerAngles = new Vector3(10f, 0f, 0f);
+                canPickUp = true;
+            }
+        }
+    }
+
+    void Drop()
+    {
+        currentWeapon.transform.parent = null;
+        currentWeapon.GetComponent<Rigidbody>().isKinematic = false;
+        currentWeapon.GetComponent<Collider>().isTrigger = false;
+        canPickUp = false;
+        currentWeapon = null;
+    }
+}
