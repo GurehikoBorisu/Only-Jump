ed0ad133af1d165920f3d10644ac4907bfc6b884
Yoroslav <140841390+Yoroslav@users.noreply.github.com>
2023-09-24 18:21:10 +0300

    о

--- Assets/Scripts/FirstPersonPlayer/PlayerMovement1.cs
+++ Assets/Scripts/FirstPersonPlayer/PlayerMovement1.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class PlayerMovement1 : MonoBehaviour
 {
     public CharacterController controller;
+<<<<<<< HEAD
 
     public float gravity = -9.8f;
     Vector3 velocity;
 
+=======
+    Vector3 velocity;
+    public float gravity;
+>>>>>>> parent of 59de4f1 (Revert "PlayerController")
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+<<<<<<< HEAD
         GroundCheckRadius = GroundCheck.GetComponent<SphereCollider>().radius;
         TopCheckRadius = TopCheck.GetComponent<SphereCollider>().radius;
         realSpeed = speed;
+=======
+        realSpeed = moveSpeed;
+>>>>>>> parent of 59de4f1 (Revert "PlayerController")
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD
         Walk();
         Run();
         Jump();
@@ -30,6 +40,16 @@ public class PlayerMovement1 : MonoBehaviour
 
     public Vector3 moveVector;
     public float speed = 10f;
+=======
+        Jump();
+        Walk();
+        SquatCheck();
+        CheckingGround();
+    }
+
+    public Vector3 moveVector;
+    public float moveSpeed = 10f;
+>>>>>>> parent of 59de4f1 (Revert "PlayerController")
     private float realSpeed;
     public float fastSpeed = 20f;
     void Walk()
@@ -38,15 +58,25 @@ public class PlayerMovement1 : MonoBehaviour
         {
             velocity.y = -2f;
         }
+<<<<<<< HEAD
         moveVector.x = Input.GetAxisRaw("Horizontal");
         moveVector.z = Input.GetAxisRaw("Vertical");
 
         Vector3 move = transform.right * moveVector.x + transform.forward * moveVector.z;
         controller.Move(move * realSpeed * Time.deltaTime);
+=======
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveVector.x + transform.forward * moveVector.z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+>>>>>>> parent of 59de4f1 (Revert "PlayerController")
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
 
+<<<<<<< HEAD
     public float jumpHeight = 3;
     void Jump()
     {
@@ -56,6 +86,8 @@ public class PlayerMovement1 : MonoBehaviour
         }
     }
 
+=======
+>>>>>>> parent of 59de4f1 (Revert "PlayerController")
     public void Run()
     {
         if (Input.GetKey(KeyCode.RightShift) || Input.GetKey(KeyCode.LeftShift) && isGround && !jumpLock)
@@ -64,7 +96,20 @@ public class PlayerMovement1 : MonoBehaviour
         }
         else
         {
+<<<<<<< HEAD
             realSpeed = speed;
+=======
+            realSpeed = moveSpeed;
+        }
+    }
+
+    public float jumpHeight = 3f;
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround && !jumpLock)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+>>>>>>> parent of 59de4f1 (Revert "PlayerController")
         }
     }
 
@@ -75,6 +120,7 @@ public class PlayerMovement1 : MonoBehaviour
 
     void SquatCheck()
     {
+<<<<<<< HEAD
         if ((Input.GetKey(KeyCode.C) && isGround))
         {
             jumpLock = true;
@@ -95,5 +141,28 @@ public class PlayerMovement1 : MonoBehaviour
     void CheckGround()
     {
         isGround = Physics.CheckSphere(GroundCheck.position, GroundCheckRadius, Ground);
+=======
+        if (Input.GetKey(KeyCode.C) && isGround)
+        {
+            jumpLock = true;
+            controller.height = 0.6f;
+            controller.center = new Vector3(0, -1.2f, 0);
+        }
+        else if(!Physics.CheckSphere(TopCheck.position, TopCheckRadius, Roof))
+        {
+            jumpLock = false;
+            controller.height = 3.6f;
+            controller.center = new Vector3(0, 0, 0);
+        }
+    }
+    public bool isGround;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    private float groundDistane = 0.4f;
+
+    void CheckingGround()
+    {
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistane, groundMask);
+>>>>>>> parent of 59de4f1 (Revert "PlayerController")
     }
 }
