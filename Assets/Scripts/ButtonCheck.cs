6388fc6df0e803ae76675dfedd51786d95887c49
GreckoBorys <143785076+GreckoBorys@users.noreply.github.com>
2023-10-19 18:24:29 +0200

    Deleted script MainMenu
    Deleted script MainMenu

--- Assets/Scripts/ButtonCheck.cs
+++ Assets/Scripts/ButtonCheck.cs
@@ -7,21 +7,21 @@ public class ButtonCheck : MonoBehaviour , IPointerEnterHandler
 {
     public int x, y, z, w, q;
     public GameObject eventSystem;
-    MainMenu menu;
+    //MainMenu menu;
     void Start()
     {
-        menu = eventSystem.GetComponent<MainMenu>();
+        //menu = eventSystem.GetComponent<MainMenu>();
     }
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
         if(gameObject.tag == "LoadLevelButton")
         {
-            menu.z = z;
+            //menu.z = z;
         }
         else if (gameObject.tag == "OpenAnotherPanelButton")
         {
-            menu.x = x;
-            menu.y = y;
+            //menu.x = x;
+            //menu.y = y;
         }
     }
 }
