cdf466e4f1a77fc307f17103fd09fb45ee5e14cd
SerogaVoin <sergvoin0904@gmail.com>
2023-11-10 13:05:46 +0200

    Added timeGame script, timeAvard script,
    ButtonRayDoors script

---
+++ Assets/Scripts/LogicGameScript/ButtonRaayDoorsOn.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonRaayDoorsOn : MonoBehaviour
+{
+    private float distance = 3f;
+    public int sceneCount;
+
+    public GameObject doors;
+
+    void OnMouseDown()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, distance) && tag == "DoorsOn")
+        {
+            Destroy(doors);
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneCount);
+        }
+    }
+}
--- Assets/Scripts/TimeGame.cs
+++ Assets/Scripts/TimeGame.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float timeGame;
+
+    int hourse;
+    int minutes;
+
+    public Text timeText;
+
+    private void Awake()
     {
-        
+        if (PlayerPrefs.HasKey("Minutes"))
+            minutes = PlayerPrefs.GetInt("Minutes");
+
+        if (PlayerPrefs.HasKey("Hourse"))
+            hourse = PlayerPrefs.GetInt("Hourse");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        timeGame += Time.deltaTime;
+
+        hourse = (int)timeGame / 3600;
+        minutes = (int)timeGame % 3600 / 60;
+
+        timeText.text = $"{hourse}.{minutes}";
+
+        PlayerPrefs.SetInt("Minutes", minutes);
+        PlayerPrefs.SetInt("Hourse", hourse);
+
     }
+
 }
