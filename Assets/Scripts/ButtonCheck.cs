9dbb6b5602080633895bf4bd50e9b0b641a72b3b
GreckoBorys <143785076+GreckoBorys@users.noreply.github.com>
2023-11-24 18:10:46 +0100

    Update MainMenu + LogicLevel
    Update MainMenu + LogicLevel

--- Assets/Scripts/ButtonCheck.cs
+++ Assets/Scripts/ButtonCheck.cs
@@ -5,23 +5,21 @@ using UnityEngine.EventSystems;
 
 public class ButtonCheck : MonoBehaviour , IPointerEnterHandler
 {
-    public int x, y, z, w, q;
     public GameObject eventSystem;
+    public int levelNumber;
+    LoadLevel loadLevel;
     //MainMenu menu;
     void Start()
     {
+        loadLevel = eventSystem.GetComponent<LoadLevel>();
         //menu = eventSystem.GetComponent<MainMenu>();
     }
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
         if(gameObject.tag == "LoadLevelButton")
         {
+            loadLevel.levelNumber = levelNumber;
             //menu.z = z;
-        }
-        else if (gameObject.tag == "OpenAnotherPanelButton")
-        {
-            //menu.x = x;
-            //menu.y = y;
         }
     }
 }
